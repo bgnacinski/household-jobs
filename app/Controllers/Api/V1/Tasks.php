7cb436cfbd2f51b7da067dabcaf30e7cbcd78450
asc3rr <borysgnacinski@gmail.com>
2022-07-31 16:38:43 +0200

    added creating and getting task by id

--- app/Controllers/Api/V1/Tasks.php
+++ app/Controllers/Api/V1/Tasks.php
@@ -31,4 +31,46 @@ class Tasks extends ResourceController
             return $this->failNotFound();
         }
     }
+
+    public function show($id = null){
+        if(is_null($id)){
+            return $this->failNotFound();
+        }
+
+        $model = new TasksModel();
+        $result = $model->getTaskByID($id);
+
+        if(!is_null($result)){
+            $response = [
+                "status" => "success",
+                "data" => $result
+            ];
+
+            return $this->respond($response);
+        }
+        else{
+            return $this->failNotFound();
+        }
+    }
+
+    public function create(){
+        $input = [
+            "name" => $this->request->getPost("name"),
+            "duration" => $this->request->getPost("duration"),
+            "is_special" => $this->request->getPost("is_special")
+        ];
+
+        print_r($input);
+
+        $model = new TasksModel();
+        $response = $model->addTask($input);
+
+        switch($response["status"]){
+            case "success":
+                return $this->respondCreated($response["data"]);
+
+            default:
+                return $this->failValidationErrors($response["errors"]);
+        }
+    }
 }
--- app/Models/TasksModel.php
+++ app/Models/TasksModel.php
@@ -10,6 +10,7 @@ class TasksModel extends Model
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $allowedFields    = ["id", "name", "duration", "is_special", "created_at"];
+    protected $returnType = \App\Entities\Task::class;
 
     // Dates
     protected $useTimestamps = false;
@@ -19,8 +20,23 @@ class TasksModel extends Model
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules      = [
+        "name" => "required",
+        "duration" => "required|integer",
+        "is_special" => "required"
+    ];
+    protected $validationMessages   = [
+        "name" => [
+            "required" => "Name field is required."
+        ],
+        "duration" => [
+            "required" => "Duration field is required.",
+            "integer" => "Duration value must be an integer."
+        ],
+        "is_special" => [
+            "required" => "Is special field is required."
+        ]
+    ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
@@ -43,5 +59,30 @@ class TasksModel extends Model
 
     public function getAllTasks(int $offset, int $limit){
         return $this->findAll($limit, $offset);
+    }
+
+    public function getTaskByID(int $id){
+        return $this->find($id);
+    }
+
+    public function addTask(array $input){
+        $val_result = $this->validate($input);
+
+        if($val_result){
+            $this->save($input);
+
+            $created_object = $this->getTaskByID($this->getInsertID());
+
+            return [
+                "status" => "success",
+                "data" => $created_object
+            ];
+        }
+        else{
+            return [
+                "status" => "valerr",
+                "errors" => $this->errors()
+            ];
+        }
     }
 }
